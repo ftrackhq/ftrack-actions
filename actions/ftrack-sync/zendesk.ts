ab4db03f9c6efd8c08f81f8da83fb319f3dc3ca5
Jimmy Callin <jimmy.callin@gmail.com>
2023-09-22 14:02:59 +0200

    feat: add source to release notes (#21)

--- actions/ftrack-sync/zendesk.ts
+++ actions/ftrack-sync/zendesk.ts
@@ -1,6 +1,5 @@
 // Sends a request to Zendesk to create a new article draft
 
-const ZENDESK_BASE_URL = "https://backlight-support-staging.zendesk.com";
 const ZENDESK_USERNAME = process.env.ZENDESK_USERNAME;
 const ZENDESK_API_KEY = process.env.ZENDESK_API_KEY;
 
@@ -11,18 +10,21 @@ const headers = {
   ).toString("base64")}`,
 };
 
-export async function getArticle(articleId: string) {
+export async function getArticle(baseUrl: string, articleId: string) {
   return (
-    await fetch(
-      `${ZENDESK_BASE_URL}/api/v2/help_center/articles/${articleId}`,
-      { headers },
-    )
+    await fetch(`${baseUrl}/api/v2/help_center/articles/${articleId}`, {
+      headers,
+    })
   ).json();
 }
 
-export async function updateArticle(body: string, articleId: string) {
+export async function updateArticle(
+  baseUrl: string,
+  body: string,
+  articleId: string,
+) {
   const result = await fetch(
-    `${ZENDESK_BASE_URL}/api/v2/help_center/articles/${articleId}/translations/en-us`,
+    `${baseUrl}/api/v2/help_center/articles/${articleId}/translations/en-us`,
     { method: "PUT", headers, body: JSON.stringify({ translation: { body } }) },
   );
 
